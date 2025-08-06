63d73f121d58edad9186dfca97b4a9271685457d
yohannes444 <yohannesmulat444@gmail.com>
2025-08-06 19:59:38 +0300

    fix 1

--- src/pages/teacher/TeacherGrades.tsx
+++ src/pages/teacher/TeacherGrades.tsx
@@ -205,10 +205,8 @@ export default function TeacherGrades() {
       return (
         <MainLayout requiredRole="teacher">
           <Box m={2}>
-            <Typography color="error">
             <CircularProgress size={60} thickness={4} />
               
-            </Typography>
             <Button
               variant="outlined"
               color="primary"
